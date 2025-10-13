832924682e75abdf6d95cdc548036166ee0045af
Valery-115 <Vcamargo8813@gmail.com>
2025-10-13 13:20:11 -0600

    123

--- spif/settings.py
+++ spif/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'spif',
         'USER': 'spif_user',
-        'PASSWORD': 'Vale#98z',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
