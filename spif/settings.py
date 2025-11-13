661e9c8b2f23ec7200342f9f7da60fb9be892126
Valery-115 <Vcamargo8813@gmail.com>
2025-11-13 13:18:00 -0700

    Por mientras_1

--- spif/settings.py
+++ spif/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'spif.wsgi.application'
 #    }
 #else:
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
