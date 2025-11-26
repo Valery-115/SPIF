a7fc6b003572bd6a13f854444026666e04e1f482
Valery-115 <Vcamargo8813@gmail.com>
2025-11-26 13:01:26 -0700

    Error con el if de DEBUG_DB

--- spif/settings.py
+++ spif/settings.py
@@ -80,26 +80,16 @@ WSGI_APPLICATION = 'spif.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 #BD del Servidor
-if not DEBUG:
-    # BD del Servidor (PostgreSQL) - Lee los valores del entorno
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB', 'spif'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_HOST'),
-            'PORT': os.environ.get('POSTGRES_PORT'),
-        }
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'spif'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
-else:
-    # BD Local (SQLite3)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
