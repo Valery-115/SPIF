4254e7e203d8de6ee6e014a3aef25e7dc1538b7c
Valery-115 <Vcamargo8813@gmail.com>
2025-11-26 12:09:15 -0700

    Prueba de las licencias de la BD en el gunicorn.service

--- spif/settings.py
+++ spif/settings.py
@@ -80,16 +80,26 @@ WSGI_APPLICATION = 'spif.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 #BD del Servidor
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spif',
-        'USER': 'spif_user',
-        'PASSWORD': 'temporal',
-        'HOST': '10.236.62.44',
-        'PORT': '5434',
+if not DEBUG:
+    # BD del Servidor (PostgreSQL) - Lee los valores del entorno
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'spif'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
+            'PORT': os.environ.get('POSTGRES_PORT'),
+        }
     }
-}
+else:
+    # BD Local (SQLite3)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
