a1d55480f2a6eefa73115ebd7f83379188449b4b
Valery-115 <Vcamargo8813@gmail.com>
2026-01-14 14:58:03 -0700

    kill me pls

--- apps/captura_contrato/admin.py
+++ apps/captura_contrato/admin.py
@@ -5,15 +5,15 @@ from .models import Contrato, Proveedor
 #Basic
 #admin.site.register(Contrato)
 #Personalizada
-#@admin.register(Contrato)
-#class ContratoAdmin(admin.ModelAdmin):
+@admin.register(Contrato)
+class ContratoAdmin(admin.ModelAdmin):
     #Definir que columnas son visibles en la BD de Admin.
-    #list_display=['numero_contrato', 'id_proveedor']
+    list_display=['numero_contrato', 'id_proveedor']
 
 #Basic
 #admin.site.register(Proveedor)
 #Personalizada
-#@admin.register(Proveedor)
-#class ProveedorAdmin(admin.ModelAdmin):
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
     #Definir que columnas son visibles en la BD de Admin.
-    #list_display=['id_proveedor', 'nombre_proveedor', 'fecha_baja']
+    list_display=['id_proveedor', 'proveedor', 'fecha_baja']
